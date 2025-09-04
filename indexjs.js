7f1407b767792ecc7af35b3ee59ef76bd89a2e96
Nipun Nirmal Samarathunga <141556293+nipunnirmal21@users.noreply.github.com>
2025-09-04 22:25:42 +0530

    cardiology js

---
+++ indexjs.js
@@ -0,0 +1,6 @@
+const departmentsData = {
+            cardiology: {
+                title: 'Cardiology',
+                description: 'Our Cardiology department provides specialized care for heart health. Our expert cardiologists diagnose and treat a wide range of cardiovascular conditions, including coronary artery disease, heart failure, and arrhythmias.',
+                services: ['ECG and EKG', 'Stress Testing', 'Angioplasty and Stenting', 'Pacemaker Implantation', 'Heart Failure Management']
+            },
